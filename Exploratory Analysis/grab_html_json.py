33af41f35b15bdcf314fef4ede3980f1a0fb1d5a
David Spellman <david_spllmn@yahoo.com>
2013-04-06 16:35:05 -0500

    Parsing is done...still need to clean and normalize the keys

--- Exploratory Analysis/grab_html_json.py
+++ Exploratory Analysis/grab_html_json.py
@@ -2,47 +2,57 @@ import urllib
 import json
 from collections import defaultdict
 from BeautifulSoup import BeautifulSoup
+import re
+
+regex_1 = re.compile("(?<=settings\s=\s)[\w\s\S]+(?=\swindow.np)")
 
 class HTML_json:
 	features_count = defaultdict(int)
 	
 def clean(js):
-	print js.replace('\n', '').replace('\t', ' ').replace(';', '')
+	return js.replace('\n', '').replace('\t', ' ').replace(';', '').replace('\'', '"')
+def parse_1(line):
+	list = regex_1.findall(line)
+	if len(list) > 0:
+		return json.loads(list[0])
+	else:
+		return None
+		
+def parse_simple(line):
+	l = line.partition('=')[2].strip()
+	return json.loads(l)
 	
+def parse_var(line):
+	l = line.partition('=')[2].strip()
+	if "friend" in line:
+		return json.loads('{"friendCount" : ' + l + '}')
+	else:
+		return json.loads('{"deviceType" : ' + l + '}')
+
 def grab_HTML(link):
 	f = urllib.urlopen(link)
 	s = f.read()
 	f.close()
 	soup = BeautifulSoup(''.join(s))
 	js= soup.findAll('script')
-	found_crit = False
 	for j in js:
 		try:
-			mystring = j.text.encode('ascii')
+			mystring = j.text.encode('utf-8')
 			if "window.np.settings = " in mystring:
-				print "found 1 \n"
-				clean(mystring)
+				print parse_1(clean(mystring))
 			if "window.np.share_data = " in mystring:
-				print "found 2 \n"
-				clean(mystring)
+				print parse_simple(clean(mystring))
 			if "window.np.baked_data =" in mystring:
-				print "found 3 \n"
-				found_crit = True
-				clean(mystring)
+				print parse_simple(clean(mystring))
 			if "deviceType =" in mystring:
-				print "found 4 \n"
-				clean(mystring)
+				print parse_var(clean(mystring))
 			if "window.np.settings.user_settings =" in mystring:
-				print "found 5 \n"
-				clean(mystring)
+				print parse_simple(clean(mystring))
 		 	if "window.np.friendCount =" in mystring:
-				print "found 6 \n"
-				clean(mystring)
-			#print mystring + '\n\n'
-		except:
+				print parse_var(clean(mystring))
+		except Exception,e: 
+			print str(e) 
 			continue
-	print found_crit, link
-	print '\n\n\n\n'
 
 json_data = open("link_json.txt")
 for line in json_data:
