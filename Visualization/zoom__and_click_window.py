acdfbfeec18aadd8ae3f9b081e459d12bf4e8a81
David Spellman <david_spllmn@yahoo.com>
2013-04-17 20:07:25 -0500

    started dummy interaction and concepts. will explain when we meet

--- Visualization/zoom__and_click_window.py
+++ Visualization/zoom__and_click_window.py
@@ -1,25 +1,13 @@
-"""
-This example shows how to connect events in one window, for example, a mouse
-press, to another figure window.
-
-If you click on a point in the first window, the z and y limits of the
-second will be adjusted so that the center of the zoom in the second
-window will be the x,y coordinates of the clicked point.
-
-Note the diameter of the circles in the scatter are defined in
-points**2, so their size is independent of the zoom
-"""
-
 import math
 import pylab
 import matplotlib
 from matplotlib.pyplot import figure, show
+from matplotlib.widgets import Slider, Button, RadioButtons
 import numpy
-
+import matplotlib.lines as lines
 import math
 
-import pylab
-import matplotlib
+from pylab import *
 
 
 class AnnoteFinder:
@@ -65,15 +53,64 @@ class AnnoteFinder:
 			  if  clickX-self.xtol < x < clickX+self.xtol and  clickY-self.ytol < y < clickY+self.ytol :
 				print "TRUE", x, y
 
+				
 figsrc = figure()
-axsrc = figsrc.add_subplot(211, xlim=(0,1), ylim=(0,1), autoscale_on=False)									
+#left, bottom
+a = figtext(.4,.4, "TEST") 
+a.set_text("PASSED")
+figsrc.subplots_adjust(bottom = .5, left = .05, right = .95, top = .95)
+axsrc = figsrc.add_subplot(111, xlim=(0,1), ylim=(0,1), autoscale_on=False)									
 axsrc.set_title('Right Click to Zoom')
-x,y,c = numpy.random.rand(3,20000)
+x,y,c = numpy.random.rand(3,2000)
+
+v_line = axvline(x = .5, linewidth = 4, color='r')
+
+h_line = axhline(y=.5, linewidth = 4, color = 'b')
+
 
 axsrc.scatter(x,y,c=c)
 
-af = AnnoteFinder(x,y, None, .0003,.0003)
+af = AnnoteFinder(x,y, None, .0004,.0004)
 
 figsrc.canvas.mpl_connect('button_press_event', af)
+#left, bottom, width, height
+
+speed_slider = axes([0.05, 0.1, 0.2, 0.03])
+dist_slider = axes([0.05,0.14,0.2,0.03])
+emotion_ax = axes([0.05, 0.3, 0.1, 0.15])
+weather_ax = axes([.16,.3,.1,.15])
+terrain_ax = axes([.27, .3, .1, .15])
+s_speed = Slider(speed_slider, "Speed", 0, 1, valinit = .5)
+s_distance = Slider(dist_slider, "Distance", 0, 1, valinit = .5)
+emotion_radio = RadioButtons(emotion_ax, ('tired','so_so','great','injured','amped','unstopable'))
+weather_radio = RadioButtons(weather_ax, ('sunny','cloudy','partly_sunny','amped','rainy','snowy'))
+terrain_radio = RadioButtons(terrain_ax, ('trail','treadmill','beach','road','amped','track'))
+
+
+def update_speed(val):
+	v_line.set_xdata(val)
+	a.set_text(val)
+	figsrc.canvas.draw()
+	
+def update_distance(val):
+	h_line.set_ydata(val)
+	figsrc.canvas.draw()
+	
+def handle_emotion(label):
+	print label
+
+def handle_weather(label):
+	print label
+	
+def handle_terrain(label):
+	print label
+	
+s_speed.on_changed(update_speed)
+s_distance.on_changed(update_distance)
+emotion_radio.on_clicked(handle_emotion)
+weather_radio.on_clicked(handle_weather)
+terrain_radio.on_clicked(handle_terrain)
+
 show()
+
 
